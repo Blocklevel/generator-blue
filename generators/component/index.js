cb7aae8a409bf8da971050fb9cf36c1813497dff
Matteo Gabriele <matteo@blocklevel.nl>
2016-11-25 23:52:33 +0100

    feat(component): add component generator

---
+++ generators/component/index.js
@@ -0,0 +1,50 @@
+const generator = require('yeoman-generator')
+const _ = require('lodash')
+const chalk = require('chalk')
+const utils = require('../utils')
+const moment = require('moment');
+
+const ComponentGenerator = module.exports = generator.Base.extend({
+  prompting: function () {
+    return this.prompt([
+      {
+        type: 'input',
+        name: 'filename',
+        message: 'What\'s the name of your component?',
+        default: 'my-component'
+      },
+      {
+        type: 'list',
+        name: 'basic',
+        message: 'Do you want a basic structure?',
+        choices: [
+          {
+            name: "No, thanks!",
+            value: false
+          },
+          {
+            name: "Oh yes!",
+            value: true
+          }
+        ],
+        default: 0
+      }
+    ]).then((answer) => {
+      this._generateComponent(answer)
+    })
+  },
+  _generateComponent: function ({ filename, basic }) {
+    const user = utils.getUser()
+    const name = _.kebabCase(filename)
+    const tplPath = '../../templates/component'
+    const distPath = 'src/app/component'
+    const componentStructure = ['js', 'vue', 'css']
+
+    _.each(componentStructure, (type) => {
+      const file = this.templatePath(`${tplPath}/index.${type}`)
+      const dest = this.destinationPath(`${distPath}/${name}/${name}.${type}`)
+
+      this.fs.copyTpl(file, dest, { name, user, basic })
+    })
+  }
+})
---
+++ generators/templates/component/index.js
@@ -0,0 +1,29 @@
+/**
+ * @name <%= name %> component
+ * @author <%= user.name %> <<%= user.email %>>
+ */
+export default {
+  name: '<%= name %>'<% if (basic) { %>,
+
+  data () {
+    return {
+
+    }
+  },
+
+  watch: {
+
+  },
+
+  methods: {
+
+  },
+
+  computed: {
+
+  },
+
+  mounted () {
+
+  }<% } %>
+}
